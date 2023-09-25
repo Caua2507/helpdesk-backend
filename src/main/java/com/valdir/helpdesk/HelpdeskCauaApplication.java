212006959339277e8bc2024160151ced67f0de0a
Caua2507 <cauarodsilvayt@gmail.com>
2023-09-25 17:30:56 -0300

    projeto iniciado

---
+++ src/main/java/com/valdir/helpdesk/HelpdeskCauaApplication.java
@@ -0,0 +1,13 @@
+package com.valdir.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskCauaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskCauaApplication.class, args);
+	}
+
+}
