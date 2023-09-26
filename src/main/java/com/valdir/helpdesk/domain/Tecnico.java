716a827917931c613a93f2108a7996c930734b83
Caua2507 <cauarodsilvayt@gmail.com>
2023-09-26 14:08:51 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/valdir/helpdesk/domain/Tecnico.java
+++ src/main/java/com/valdir/helpdesk/domain/Tecnico.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.valdir.helpdesk.domain.enums.Perfil;
+
 @Entity
 public class Tecnico extends Pessoa {
 	private static final long serialVersionUID = 1L;
@@ -15,6 +17,12 @@ public class Tecnico extends Pessoa {
 
 	public Tecnico() {
 		super();
+		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
@@ -25,8 +33,5 @@ public class Tecnico extends Pessoa {
 		this.chamados = chamados;
 	}
 
-	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
-		super(id, nome, cpf, email, senha);
-	}
 	
 }
